72002bbb1488a05ae0015fc3139d460961ab091a
xingdadi <dadixing@gmail.com>
2014-02-01 13:24:58 -0500

    test
    test

---
+++ main_code.R
@@ -0,0 +1,15 @@
+#put the folder "myApp" in the wroking directory--getwd()
+
+library(shiny)
+
+library(spam)
+library(MBA)
+library(fields)
+library(rgl)
+library(graphics)
+library(XML)
+library(ggmap)
+library(Rcpp)
+
+
+runApp("myApp02")
